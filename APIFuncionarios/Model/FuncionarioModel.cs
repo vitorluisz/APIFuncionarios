8e6b3d3d61a0fb5913bbdd1bbcc2b4e6de56bd07
Vitor <vitorlzampronha@gmail.com>
2024-09-30 18:57:38 -0300

    Adicionar arquivos de projeto.

---
+++ APIFuncionarios/Model/FuncionarioModel.cs
@@ -0,0 +1,19 @@
+﻿using APIFuncionarios.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIFuncionarios.Model
+{
+    public class FuncionarioModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Nome { get; set; }
+        public int Sobrenome { get; set; }
+        public DepartamentoEnum Departamento { get; set; }
+        public bool Ativo { get; set; }
+        public TurnoEnum Turno { get; set; }
+        public DateTime DataDeCriacao { get; set; } = DateTime.Now;
+        public int DataDeAlteracao { get; set; }
+
+    }
+}
